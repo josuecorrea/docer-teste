71e73e43ae3df33c8c279ec38c92c40af52aed4e
Josue Correa <josuejtc@gmail.com>
2020-03-20 19:55:02 -0300

    ElasticApm

--- TesteDocker/Startup.cs
+++ TesteDocker/Startup.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Elastic.Apm.AspNetCore;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
@@ -34,6 +35,8 @@ namespace TesteDocker
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.UseElasticApm(Configuration);
 
             app.UseRouting();
 
